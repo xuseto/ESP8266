dd085a07b0109fb17691a3252d6fa48a7023f23a
xuseto <jesniehe85@gmail.com>
2021-01-29 20:27:21 +0100

    create new branch "test_heartbeat"

--- WIFI_Client_GET/src/main.c
+++ WIFI_Client_GET/src/main.c
@@ -1,43 +1,47 @@
-/**
+/*******************************************************************************
  * @file main.c
- * @version 4.1.0
+ * @author xuseto
+ * @version 0.0.0.0.1
+ * @date Creation: 29/01/2021
+ * @date Last modification 29/01/2021 
+ * @brief
+ * @par
+ *  COPYRIGHT NOTICE: (c) 2021 Xuseto.
+ *  All rights reserved
+ *******************************************************************************
  *
- * @section License
- * Copyright (C) 2015-2016, Erik Moqvist
+ *  @addtogroup MAIN
+ *  @{
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * This file is part of the Simba project.
  */
 
+/* Includes ------------------------------------------------------------------*/
 #include "simba.h"
 
+/* Estructuras ---------------------------------------------------------------*/
+
+/* Variables -----------------------------------------------------------------*/
+
+/* Funciones Privadas --------------------------------------------------------*/
+
+/* Funciones Públicas --------------------------------------------------------*/
+/**
+ * @brief Main. inicio del código
+ * @return int
+ */
 int main()
 {
-    struct pin_driver_t led;
-
-    /* Start the system. */
-    sys_start();
-
-    /* Initialize the LED pin as output and set its value to 1. */
-    pin_init(&led, &pin_led_dev, PIN_OUTPUT);
-    pin_write(&led, 1);
-
     while (1) {
-        /* Wait half a second. */
-        thrd_sleep_us(500000);
 
-        /* Toggle the LED on/off. */
-        pin_toggle(&led);
     }
 
     return (0);
 }
+
+
+
+/**
+ *  @}
+ */
+
+/******************** (C) COPYRIGHT xuseto*****END OF FILE ********************/
